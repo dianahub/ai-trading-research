c253619a20210da08009269ec28d41c202026899
Diana Castillo <dianahelene@gmail.com>
2026-04-26 21:47:30 -0400

    added prod.starsignal.io

--- backend/main.py
+++ backend/main.py
@@ -50,6 +50,7 @@ app.add_middleware(
         "https://starsignal-waitlist.vercel.app",
         "https://starsignal.io",
         "https://www.starsignal.io",
+        "https://prod.starsignal.io",
         "https://staging.starsignal.io",
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
